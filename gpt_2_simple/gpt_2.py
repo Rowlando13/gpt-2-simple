ef441391cb4f8b836adc4bb5ff6c7ea60789abd5
Rowlando13 <67291205+Rowlando13@users.noreply.github.com>
2020-08-03 11:21:29 -0400

    Update gpt_2.py

--- gpt_2_simple/gpt_2.py
+++ gpt_2_simple/gpt_2.py
@@ -236,7 +236,7 @@ def finetune(sess,
         summary_loss = tf.compat.v1.summary.scalar('loss', loss)
 
     summary_log = tf.compat.v1.summary.FileWriter(checkpoint_path)
-
+print(summary_log)
     saver = tf.compat.v1.train.Saver(
         var_list=all_vars,
         max_to_keep=max_checkpoints)
